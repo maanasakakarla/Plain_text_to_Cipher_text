37d7a76bceb5c12d0aafed726b94aa6a03bc1377
maanasakakarla <112385380+maanasakakarla@users.noreply.github.com>
2023-12-19 11:46:54 +0530

    Create Caesercipher.java

--- Caesercipher.java
+++ Caesercipher.java
@@ -1,54 +1,56 @@
-import java.util.*;
-import java.io.*;
-public class Caesercipher
-{
- public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
- public static String encrypt(String ptext, int cserkey)
- {
- String ctext = "";
- for (int i = 0; i < ptext.length(); i++)
- {
- int plainnumeric = ALPHABET.indexOf(ptext.charAt(i));
- int ciphernumeric = (plainnumeric+cserkey) % 26;
- char cipherchar = ALPHABET.charAt(ciphernumeric);
- ctext += cipherchar;
- }
- return ctext;
- }
- public static String decrypt(String ctext, int cserkey)
- {
- String ptext = "";
- for (int i = 0; i < ctext.length(); i++)
- {
- int ciphernumeric = ALPHABET.indexOf(ctext.charAt(i));
- int plainnumeric= (ciphernumeric-cserkey) % 26;
- if (plainnumeric < 0)
- {
- plainnumeric = ALPHABET.length() + plainnumeric;
- }
- char plainchar = ALPHABET.charAt(plainnumeric);
- ptext += plainchar;
- }
- return ptext;
- }
- public static void main(String[] args)
- throws IOException
- {
- BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- System.out.println("Enter the PLAIN TEXT for Encryption: ");
- String plaintext = new String();
- String ciphertext = new String();
- String key;
- int cserkey;
- plaintext = br.readLine();
- System.out.println("Enter the CAESERKEY between 0 and 25:");
- key = br.readLine();
- cserkey = Integer.parseInt(key);
- System.out.println("ENCRYPTION");
- ciphertext = encrypt(plaintext,cserkey);
- System.out.println("CIPHER TEXT :"+ ciphertext);
- System.out.println("DECRYPTION");
- plaintext = decrypt(ciphertext,cserkey);
- System.out.println("PLAIN TEXT :" + plaintext);
- }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CaeserCipher {
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+    public static String encrypt(String plaintext, int caeserKey) {
+        StringBuilder ciphertext = new StringBuilder();
+        
+        for (int i = 0; i < plaintext.length(); i++) {
+            int plainNumeric = ALPHABET.indexOf(plaintext.charAt(i));
+            int cipherNumeric = (plainNumeric + caeserKey) % 26;
+            char cipherChar = ALPHABET.charAt(cipherNumeric);
+            ciphertext.append(cipherChar);
+        }
+        
+        return ciphertext.toString();
+    }
+
+    public static String decrypt(String ciphertext, int caeserKey) {
+        StringBuilder plaintext = new StringBuilder();
+        
+        for (int i = 0; i < ciphertext.length(); i++) {
+            int cipherNumeric = ALPHABET.indexOf(ciphertext.charAt(i));
+            int plainNumeric = (cipherNumeric - caeserKey) % 26;
+            
+            if (plainNumeric < 0) {
+                plainNumeric = ALPHABET.length() + plainNumeric;
+            }
+            
+            char plainChar = ALPHABET.charAt(plainNumeric);
+            plaintext.append(plainChar);
+        }
+        
+        return plaintext.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter the PLAIN TEXT for Encryption:");
+        String plaintext = br.readLine();
+
+        System.out.println("Enter the CAESAR KEY between 0 and 25:");
+        int caeserKey = Integer.parseInt(br.readLine());
+
+        System.out.println("ENCRYPTION");
+        String ciphertext = encrypt(plaintext, caeserKey);
+        System.out.println("CIPHER TEXT: " + ciphertext);
+
+        System.out.println("DECRYPTION");
+        plaintext = decrypt(ciphertext, caeserKey);
+        System.out.println("PLAIN TEXT: " + plaintext);
+    }
 }
